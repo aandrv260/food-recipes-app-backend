8aad0e97c4855d02785547ed5688fe78f0c165ee
aandrv260 <angel42505@outlook.com>
2022-07-15 14:46:03 +0300

    Added allow origin support

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const app = express();
 // Configure the environment variables
 dotenv.config({ path: './config.env' });
 
+// Allow Access Origin and Methods
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application'
+  );
+  next();
+});
+
 const PORT = process.env.PORT || 4000;
 
 // Parse request body
